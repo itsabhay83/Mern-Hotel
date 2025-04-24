3aa1c33abb091da8ba158c95d946b5110ef3ad84
itsabhay83 <22bec003@nith.ac.in>
2025-04-24 19:08:52 +0530

    added the civic auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import { Routes, Route } from "react-router-dom"
-import Header from "./components/header/Header"
-import Footer from "./components/footer/Footer"
-import Home from "./pages/home/Home"
-import About from "./pages/about/About"
-import Contact from "./pages/contact/Contact"
-import Blog from "./pages/blog/Blog"
-import Profile from "./pages/profile/Profile"
-import MyBookings from './pages/myBookings/MyBookings'
-import SingleBooking from './pages/myBookings/SingleBooking'
-import Login from "./pages/login/Login"
-import Register from "./pages/register/Register"
-import Error from './components/error/Error'
-import SingleBlog from './pages/blog/SingleBlog'
-import Testimonial from './pages/testimonial/Testimonial'
-import Hotel from './pages/hotels/Hotel'
-import SingleHotel from './pages/hotels/SingleHotel'
-import Room from './pages/room/Room'
-import PrivateRoute from './PrivateRoute'
-import Success from "./pages/success/Success"
-import Cancel from "./pages/cancel/Cancel"
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Blog from "./pages/blog/Blog";
+import Profile from "./pages/profile/Profile";
+import MyBookings from './pages/myBookings/MyBookings';
+import SingleBooking from './pages/myBookings/SingleBooking';
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Error from './components/error/Error';
+import SingleBlog from './pages/blog/SingleBlog';
+import Testimonial from './pages/testimonial/Testimonial';
+import Hotel from './pages/hotels/Hotel';
+import SingleHotel from './pages/hotels/SingleHotel';
+import Room from './pages/room/Room';
+import PrivateRoute from './PrivateRoute';
+import Success from "./pages/success/Success";
+import Cancel from "./pages/cancel/Cancel";
 
+import { CivicAuthProvider } from "@civic/auth-web3/react";
 
 const App = () => {
   return (
-    <>
+    <CivicAuthProvider clientId= {import.meta.env.VITE_CIVIC_CLIENT_ID}>
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
@@ -37,23 +38,20 @@ const App = () => {
 
         <Route path='/' element={<PrivateRoute />}>
           <Route path='/room/:id' element={<Room />} />
-          
           <Route path='/profile' element={<Profile />} />
           <Route path='/myBookings' element={<MyBookings />} />
           <Route path='/myBookings/:id' element={<SingleBooking />} />
-
           <Route path='/success' element={<Success />} />
           <Route path='/cancel' element={<Cancel />} />
         </Route>
 
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
-
         <Route path='/*' element={<Error />} />
       </Routes>
       <Footer />
-    </>
-  )
-}
+    </CivicAuthProvider>
+  );
+};
 
-export default App
+export default App;
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,35 +1,9 @@
-import { useEffect, useState } from "react";
-import axiosInstance from "../../axios";
+import { useEffect } from "react";
+import { useUser } from "@civic/auth-web3/react";
 import { handleGuestUser } from "../../components/HandleGuestUser";
-import { toast } from "react-toastify";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const formSubmit = async (e) => {
-    e.preventDefault();
-
-    const user = {
-      email: email,
-      password: password,
-    };
-
-    await axiosInstance
-      .post(`api/auth/login`, user)
-      .then((res) => {
-        localStorage.clear();
-        localStorage.setItem("access_token", res.data.token);
-        localStorage.setItem("_id", res.data._id);
-        axiosInstance.defaults.headers["Authorization"] = "Bearer " + localStorage.getItem("access_token");
-        toast.success("Login Successful");
-        window.location.href = "/";
-      })
-      .catch((err) => {
-        console.log(err);
-        toast.error("Invalid email or password");
-      });
-  };
+  const { user, signIn, signOut } = useUser();
 
   useEffect(() => {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
@@ -47,50 +21,34 @@ const Login = () => {
         />
       </div>
 
-      <form onSubmit={formSubmit} className="m-6 lg:m-28 lg:py-12">
-        <div className="flex flex-col">
-          <h1 className="text-2xl lg:text-3xl font-semibold mb-7 lg:mb-10 text-[#908dcd]">
-            Sign in to your account
-          </h1>
+      <div className="m-6 lg:m-28 lg:py-12 flex flex-col">
+        <h1 className="text-2xl lg:text-3xl font-semibold mb-7 lg:mb-10 text-[#908dcd]">
+          Sign in to your account
+        </h1>
 
-          <div className="flex flex-col">
-            <span className="text-black">
-              Email address<label className="text-red-900">*</label>
-            </span>
-            <input
-              className=" border border-gray-300 py-2 rounded-md mt-2 pl-2"
-              type="email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="flex flex-col mt-4">
-            <span className="text-black">
-              Password <label className="text-red-900">*</label>
-            </span>
-            <input
-              className=" border border-gray-300 py-2 rounded-md mt-2 px-2"
-              type="password"
-              name="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-        </div>
+        {!user ? (
+          <button
+            onClick={signIn}
+            className="w-full rounded-md py-2 mt-8 bg-[#4f46e5] text-white hover:bg-[#6661c7]"
+          >
+            Sign in 
+          </button>
+        ) : (
+          <button
+            onClick={signOut}
+            className="w-full rounded-md py-2 mt-8 bg-red-500 text-white hover:bg-red-600"
+          >
+            Sign out
+          </button>
+        )}
 
-        <button className="w-full rounded-md py-2 mt-8 bg-[#4f46e5] text-white hover:bg-[#6661c7]">
-          Login
-        </button>
         <button
           onClick={handleGuestUser}
           className="w-full rounded-md py-2 mt-8 bg-[#4f46e5] text-white hover:bg-[#6661c7]"
         >
           Login as Guest
         </button>
-      </form>
+      </div>
     </section>
   );
 };
